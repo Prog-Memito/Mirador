b3ae047d4344a087be33e259821d90998f297789
Prog-Memito <sa3776530@gmail.com>
2025-01-26 18:20:00 -0300

    staff creado

--- etapa1.py
+++ etapa1.py
@@ -54,6 +54,12 @@ class Tenant(db.Model):
     # RELACIONA CON EL DEPARTAMENTO
     depto= db.relationship('Depto', backref='tenants', lazy=True)
 
+class Staff(db.Model):
+    idstaff= db.Column(db.Integer, primary_key=True)
+    s_nombre= db.Column(db.String(300), nullable=False)
+    s_apellido= db.Column(db.String(300), nullable=False)
+    funcion= db.Column(db.String(300), nullable=False)
+
 with app.app_context():
     db.create_all()
 
@@ -247,6 +253,81 @@ def eliminar_tenant(id):
     if not tenant:
         return jsonify({'mensaje': 'Arriendatario no encontrado'}),404
     db.session.delete(tenant)
+    db.session.commit()
+    return jsonify({'mensaje': 'ERAI'}),401
+
+# SISTEMA CRUD PERSONAL
+
+## CREACION DE LOS PERSONAL
+
+@app.route('/api/staff', methods=['POST'])
+@requiere_autenticacion
+def crear_staff():
+    datos = request.get_json()
+    nuevo_staff = Staff(
+        s_nombre = str(datos['nombre']),
+        s_apellido = str(datos['apellido']),
+        funcion = int(datos['funcion'])
+    )
+
+    db.session.add(nuevo_staff)
+    db.session.commit()
+    return jsonify({'mensaje': 'Personal creado', 'id':nuevo_staff.idstaff})
+
+## LECTURA DE LOS PERSONAL
+
+@app.route('/api/staff', methods=['GET'])
+@requiere_autenticacion
+def obtener_staffs():
+    staffs= Staff.query.all()
+
+    resultado= [
+        {'id':s.itstaff, 'nombre':s.s_nombre, 'apellido':s.s_apellido, 'funcion':s.funcion}
+        for s in staffs
+    ]
+    return jsonify(resultado)
+
+# LECTURA DE UN SOLO PERSONAL
+
+@app.route('/api/staff/<int:id>', methods=['GET'])
+@requiere_autenticacion
+def obtener_staff(id):
+    staff = Staff.query.get(id)
+    if not staff:
+        return jsonify({'mensaje': 'Personal no encontrado'}),404
+    return jsonify({
+        'id': staff.idstaff,
+        'nombre': staff.s_nombre,
+        'apellido': staff.s_apellido,
+        'funcion' : staff.funcion
+    })
+
+## ACTUALIZA LOS PERSONAL
+
+@app.route('/api/staff/<int:id>', methods=['PUT'])
+@requiere_autenticacion
+def actualizar_staff(id):
+    datos = request.get_json()
+    staff = Staff.query.get(id)
+    if not staff:
+        return jsonify({'mensaje': 'Personal no encontrado'}),404
+    
+    staff.s_nombre = datos.get('nombre', staff.s_nombre)
+    staff.s_apellido = datos.get('apellido', staff.s_apellido)
+    staff.funcion = datos.get('funcion', staff.funcion)
+
+    db.session.commit()
+    return jsonify({'mensaje': 'Personal modificado', 'id': staff.idstaff})
+
+## ELIMINA EL PERSONAL
+
+@app.route('/api/staff/<int:id>', methods=['DELETE'])
+@requiere_autenticacion
+def eliminar_staff(id):
+    staff = Staff.query.get(id)
+    if not staff:
+        return jsonify({'mensaje': 'Personal no encontrado'}),404
+    db.session.delete(staff)
     db.session.commit()
     return jsonify({'mensaje': 'ERAI'}),401
 
